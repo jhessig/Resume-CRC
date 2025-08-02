0a418b7143ff75de74e070198456d6a3b4135a29
Jeremy Hessig <jeremy@hessig.contact>
2025-08-02 10:57:32 -0400

    Add visitor count functionality and copyright update
    Implemented an API call to dynamically fetch and display the visitor count on the resume website. Updated copyright year to 2025.

--- resume.js
+++ resume.js
@@ -1,3 +1,22 @@
-function myFunction() {
-    document.getElementById("myBtn").addEventListener("click", function (event) {})
+async function updateVisitorCount() {
+    const API_URL = 'https://{{API_URL}}/api/visitor_count';
+    try {
+        const response = await fetch(API_URL, {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json',
+            },
+            mode: 'cors'
+        });
+
+        if (response.ok) {
+            const data = await response.json();
+            document.getElementById('count').textContent = data.count;
+        } else {
+            document.getElementById('count').textContent = 'Error loading count';
+        }
+    } catch (error) {
+        console.error('Error:', error);
+        document.getElementById('count').textContent = 'Error loading count';
+    }
 }
